dd2655e7d9fad9b620fd0613b8cdadad01926c37
Maxime Grec (Subskill) <m.grec@subskill.com>
2018-12-19 11:54:39 +0100

    add front APP and connect to star wars API

--- App/src/app/api.service.ts
+++ App/src/app/api.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from  '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+import { Observable } from  'rxjs';
+
+export  interface  Item{
+  results : Person[]
+}
+
+export  interface Person{
+    name:  string;
+    gender:  string;
+    url:  string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn:  'root'
 })
-export class ApiService {
 
-  constructor() { }
+export  class  ApiService {
+  private  dataURL:  string  =  "https://swapi.co/api/people/?format=json";
+  constructor(private  httpClient:  HttpClient) {}
+  fetch():  Observable<any>{
+    return this.httpClient.get(this.dataURL);
+  }
 }
--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from  '@angular/core';
+import {ApiService, Person} from './api.service';
+import { Item } from  './api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector:  'app-root',
+  templateUrl:  './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'App';
+export  class  AppComponent  implements  OnInit{
+  title  =  'Star Wars - PWA';
+  items:  Person[];
+  constructor(private  apiService:  ApiService){
+  }
+  ngOnInit(){
+    this.fetchData();
+  }
+  fetchData(){
+    this.apiService.fetch().subscribe((data:  Item) => {
+      this.items  =  data.results;
+    }, (err)=>{
+      console.log(err);
+    });
+  }
 }
